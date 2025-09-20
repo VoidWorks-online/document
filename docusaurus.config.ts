c0c1247f02b4121db7e3817f586cb6320f508eac
ExMikuPro39 <72144156+ExMikuPro@users.noreply.github.com>
2025-09-20 18:29:07 +0800

    fix-domain: 修改绑定域名

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://voidworks.k-on.plus',
+  url: 'https://docs.voidworks.link',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
